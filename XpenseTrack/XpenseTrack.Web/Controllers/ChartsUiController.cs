6a4b805340eb5e6974f0b6ffec95949ffb8fed6f
Ashok Kumar Karasala <akkarasala@configit.com>
2018-07-31 20:31:50 +0530

    MVC and WebApi Controllers Added

---
+++ XpenseTrack/XpenseTrack.Web/Controllers/ChartsUiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XpenseTrack.Web.Controllers
+{
+    public class ChartsUiController : Controller
+    {
+        // GET: ChartsUi
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
